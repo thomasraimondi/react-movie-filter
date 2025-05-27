1ac3a2ff42f59f6b06e60872cccf79fdd3e33d89
thomasraimondi <thomas.raimondi14@gmail.com>
2025-05-27 15:03:01 +0200

    add boostra icons in header

--- src/assets/Components/layout/Headers.jsx
+++ src/assets/Components/layout/Headers.jsx
@@ -1,16 +1,24 @@
-export default function Header({ isFormVisible, setIsFormVisible }) {
-  console.log(isFormVisible);
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-  const handleFormVisible = () => setIsFormVisible(!isFormVisible);
+export default function Header({ isAddVisible, setIsFormVisible }) {
+  const handleFormVisible = () => setIsFormVisible(!isAddVisible);
+  const handleFilterVisible = () => alert("ciao");
 
   return (
     <div className="header d-flex justify-content-between p-3">
       <div className="title">
         <h1>Movies List</h1>
       </div>
-      <div className="function">
+      <div className="function d-flex align-items-center gap-2">
+        <button className="btn btn-primary" onClick={handleFilterVisible}>
+          <i className="bi bi-funnel"></i>
+        </button>
         <button className="btn btn-primary" onClick={handleFormVisible}>
-          {isFormVisible ? "x" : "+"}
+          {isAddVisible ? (
+            <i className="bi bi-x"></i>
+          ) : (
+            <i className="bi bi-plus"></i>
+          )}
         </button>
       </div>
     </div>
